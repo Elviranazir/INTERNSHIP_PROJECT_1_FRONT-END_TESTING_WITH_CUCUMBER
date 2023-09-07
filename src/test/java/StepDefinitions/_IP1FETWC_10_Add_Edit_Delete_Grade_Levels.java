323b39b23ff8bf1c845da587322af618b1d260e8
Elvira <elvira.nzr@gmail.com>
2023-09-07 17:19:49 -0400

    last changes

---
+++ src/test/java/StepDefinitions/_IP1FETWC_10_Add_Edit_Delete_Grade_Levels.java
@@ -0,0 +1,44 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Pages.LeftNavBar;
+import Utilities.MyMethods;
+import Utilities.ParameterDriver;
+import io.cucumber.java.en.*;
+
+import java.util.List;
+
+public class _IP1FETWC_10_Add_Edit_Delete_Grade_Levels extends MyMethods {
+
+    DialogContent dialogContent = new DialogContent();
+    LeftNavBar leftNavBar = new LeftNavBar();
+
+    @And("Click on Parameters")
+    public void navigateToParameters() {
+        clickMethod(leftNavBar.parametersButton);
+    }
+
+    @Given("Click on Grade Levels")
+    public void click_on_grade_levels() {
+        leftNavBar.clickMethod(leftNavBar.gradeLevels);
+    }
+
+    @When("Fill the form with Grade Levels Data")
+    public void fill_the_form_with_grade_levels_data(io.cucumber.datatable.DataTable dataTable) {
+        List<String> dataTableList = dataTable.asList(String.class);
+
+        dialogContent.sendKeysMethod(dialogContent.nameFormGradeLevel, dataTableList.get(0));
+        dialogContent.sendKeysMethod(dialogContent.shortNameFormGradeLevel, dataTableList.get(1));
+        dialogContent.sendKeysMethod(dialogContent.orderFormGradeLevel, dataTableList.get(2));
+    }
+
+    @Given("Click on Setup")
+    public void navigate_to_setup() {
+        leftNavBar.clickMethod(leftNavBar.setUpButton);
+    }
+
+    @When("Clicked on Grade Levels Edit button")
+    public void clickedOnGradeLevelsEditButton() {
+        clickMethod(dialogContent.editGradeLevels);
+    }
+}
